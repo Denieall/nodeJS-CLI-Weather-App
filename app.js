0f296089bef870dc01edf5963acd4740aeac6a6e
denieall j <denieall.joe@gmail.com>
2018-08-16 18:51:26 +0800

    Modify app.js

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const yargs = require('yargs');
-
-const geocode = require("./geocode/geocode.js");
-const weather = require("./weather/weather.js");
+const axios = require('axios');
 
 const argv = yargs
     .options({
@@ -16,27 +14,56 @@ const argv = yargs
     .alias('help', "h")
     .argv;
 
-geocode.findLocation(encodeURIComponent(argv.address), (errMsg, res) => {
+let encodedAddress = encodeURIComponent(argv.address);
+let geocodeURL = `https://maps.googleapis.com/maps/api/geocode/json?address=%20${encodedAddress}&key=AIzaSyDXD_sLVnKn4NxZCql2bcWsUCsF4a3WiqA`;
 
-    if (errMsg == null) {
+axios.get(geocodeURL).then(
+    (response) => {
+        if (response.data.status === 'ZERO_RESULTS') {
 
-        console.log(`\nFormated Address:\n${res.fAddress}`);
-        console.log(`\nCoordinates:\n${res.latitude}, ${res.longitude}`);
+            // After throw the codes below will never run
+            throw new Error("Nothing found!");
 
-        weather.fetchWeather(res.latitude, res.longitude, (weatherErrMsg, weatherObj) => {
-            if (weatherErrMsg == null) {
-                console.log(`\nTemperature:\n${weatherObj.temperature}`);
-                console.log(`\nSummary:\n${weatherObj.summary}`);
-            } else {
-                console.log(weatherErrMsg);
-            }
-        });
+        } else if (response.data.status === 'OK') {
+            console.log("\nFormatted Address:\n" + response.data.results[0].formatted_address);
+            console.log("\nCoordinates:\n" + response.data.results[0].geometry.location.lat + ", " + response.data.results[0].geometry.location.lng);
 
+            let weatherURL = `https://api.darksky.net/forecast/14351cc4a94fc3cb6711f2b269c655b0/${response.data.results[0].geometry.location.lat},${response.data.results[0].geometry.location.lng}?units=si`
 
-    } else {
-
-        console.log(errMsg);
-
+            return axios.get(weatherURL);
+        }
     }
+).then(
+    (res) => {
+        if (res.status == 200) {
+            let temp = "Temperature is: " + res.data.currently.temperature + " °C";
+            let weatherSummary = "Weather summary: " + res.data.currently.summary;
+            let rain = "Chance of percipitation: " + res.data.currently.precipProbability;
+            let windSpeed = "Wind speed: " + res.data.currently.windSpeed + " km/h";
 
-});
+            let x = Math.max(temp.length, weatherSummary.length, rain.length, windSpeed.length);
+            let decoString = "";
+
+            for (let i = 1; i <= x; i++) {
+                 decoString = decoString + "-";
+            }
+
+            console.log("\n" + decoString + "\n");
+            console.log(temp);
+            console.log(weatherSummary);
+            console.log(rain);
+            console.log(windSpeed);
+            console.log("\n" + decoString);
+        } else {
+            console.log("Unable to fetch weather forecast!");
+        }
+    }
+).catch(
+    (error) => {
+        if (error.code === 'ENOTFOUND') {
+            console.log("Unable to connect to API server!");
+        } else {
+            console.log(error.message);
+        }
+    }
+);
